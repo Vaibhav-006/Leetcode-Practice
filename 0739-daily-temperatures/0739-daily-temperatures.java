1627932665b7ad22e5cc22080348377524334833
Vaibhav-006 <vaibhavsharmma123@gmail.com>
2026-07-14 23:51:08 +0530

    Time: 62 ms (58.82%), Space: 108 MB (7.12%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st= new Stack<>();
+        int n= temperatures.length;
+        int res[]= new int[n];
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && temperatures[i]>temperatures[st.peek()]){
+                int prev=st.pop();
+                res[prev]=i-prev;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+}
