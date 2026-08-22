63598c601ddeeb8b1095f84a45b65b686b0b96d1
Vaibhav-006 <vaibhavsharmma123@gmail.com>
2026-08-22 18:10:04 +0530

    Time: 4 ms (99.67%), Space: 63.8 MB (19.79%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,30 +1,37 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        int[] state = new int[n];
-        List<Integer> safeNodes=new ArrayList<>();
+        List<Integer> res= new ArrayList<>();
+        boolean[] safe= new boolean[graph.length];
+        boolean[] visited= new boolean[graph.length];
+        boolean[] path= new boolean[graph.length];
 
-        for (int i=0; i<n; i++) {
-            if (dfs(graph, i, state)) {
-                safeNodes.add(i);
+        for(int i=0; i<graph.length; i++){
+            if(!visited[i]){
+                dfs(i, visited, path, graph, safe);
             }
         }
-
-        return safeNodes;
+        for(int i=0; i<safe.length; i++){
+            if(safe[i]){
+                res.add(i);
+            }
+        }
+        return res;
     }
 
-    private boolean dfs(int[][] graph, int node, int[] state){
-        if (state[node]>0) {
-            return state[node]==2;
-        }
-        state[node]=1;
+    public boolean dfs(int node, boolean[] visited, boolean[] path, int[][] graph, boolean[] safe){
+        visited[node]=true;
+        path[node]=true;
 
-        for(int neighbor : graph[node]){
-            if(!dfs(graph, neighbor, state)){
-                return false;
+        for(int val : graph[node]){
+            if(!visited[val]){
+                if(dfs(val, visited, path, graph, safe)) return true;
+            }
+            else if(path[val]){
+                return true;
             }
         }
-        state[node]=2;
-        return true;
+        safe[node]=true;
+        path[node]=false;
+        return false;
     }
 }
